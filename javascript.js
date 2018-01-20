700ba2360659cf953c8865e1c532fe6604a3c1c7
Annika100 <annika.fischer97@web.de>
2018-01-20 17:27:10 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 AFRAME.registerComponent('event-animate', {
     schema: {
         target: {type: 'selector'},
+        target2: {type: 'selector'},
         aevent: {default: 'animation1'},
         triggeraction: {default: 'click' }
     },
@@ -10,10 +11,48 @@ AFRAME.registerComponent('event-animate', {
         var data= this.data;
 
         this.el.addEventListener(data.triggeraction, function () {
-
-                data.target.emit(data.aevent);
-
-
+            data.target.emit(data.aevent);
         });
     }
-}); 
+});
+
+/*
+var tick = 0;
+
+AFRAME.registerComponent('look-animate', {
+    schema: {
+        target: {type: 'selector'},
+        target2: {type: 'selector'},
+        aevent: {default: 'animation1'}
+    },
+
+    init: function() {
+        console.log('starting component look-animate..');
+
+        var data= this.data;
+        var on = 0;
+
+
+        this.el.addEventListener('mouseenter', function () {
+            on = 1;
+            setInterval(function () {
+                if(on ===1 ) tick++;
+                console.log('Tick: ' + tick);
+            }, 1000);
+        });
+
+        this.el.addEventListener('mouseleave', function () {
+            on = 0;
+            tick = 0;
+            console.log('Tick: ' + tick);
+        });
+    },
+    tick: function() {
+        var data= this.data;
+        if(tick>=6) {
+            data.target.emit(data.aevent);
+            data.target2.emit(data.aevent);
+        }
+    }
+});
+*/
