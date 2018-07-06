1f384846ee9f7a56cffad1fc31b69c9dba3c03e2
NinaGo97 <n.gollwig@t-online.de>
2018-07-06 15:18:58 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,13 +1,12 @@
-// Targets um ein Event auzulösen
-AFRAME.registerComponent('event-animate', {
+AFRAME.registerComponent('event-animate', { // A-Frame Component zur Animation auf 'click'
     schema: {
-        target: {type: 'selector'},
+        target: {type: 'selector'}, //mehrere Targets um mehrere Animationen mit einem 'click' auszulösen
         target2: {type: 'selector'},
         target3: {type: 'selector'},
         target4: {type: 'selector'},
         target5: {type: 'selector'},
         target6: {type: 'selector'},
-        aevent: {default: 'animation1'},
+        aevent: {default: 'animation1'}, // verlinkung als 'begin' in einer Animation
         triggeraction: {default: 'click' }
     },
 
@@ -29,7 +28,8 @@ AFRAME.registerComponent('event-animate', {
 
 var tick = 0;
 
-//Targets um eine Animation durch gucken auszulösen
+//Animationen nicht über 'click', sondern über 'look' auslösen
+// Funktion funktioniert, jedoch hängt sich A-Frame auf bei zu vielen Animationen und Objekten in einer Datei
 AFRAME.registerComponent('look-animate', {
     schema: {
         target: {type: 'selector'},
@@ -45,15 +45,15 @@ AFRAME.registerComponent('look-animate', {
         var on = 0;
 
 
-        this.el.addEventListener('mouseenter', function () {
+        this.el.addEventListener('mouseenter', function () { //wenn cursor auf dem Objekt
             on = 1;
             setInterval(function () {
                 if(on ===1 ) tick++;
                 console.log('Tick: ' + tick);
-            }, 1000);
+            }, 1000); // Animation wird nach n (hier 1 Sekunde) Sekunden ausgelöst
         });
 
-        this.el.addEventListener('mouseleave', function () {
+        this.el.addEventListener('mouseleave', function () { // Wenn cursor nicht mehr auf dem Objekt ist, wird tick zurückgesetzt
             on = 0;
             tick = 0;
             console.log('Tick: ' + tick);
@@ -69,43 +69,3 @@ AFRAME.registerComponent('look-animate', {
     }
 });
 
-var ticktwo = 0;
-
-AFRAME.registerComponent('look-animate-two', {
-    schema: {
-        target: {type: 'selector'},
-        target2: {type: 'selector'},
-        target3: {type: 'selector'},
-        aevent: {default: 'animation1'}
-    },
-
-    init: function() {
-        console.log('starting component look-animate-two..');
-
-        var data= this.data;
-        var ontwo = 0;
-
-
-        this.el.addEventListener('mouseenter', function () {
-            ontwo = 1;
-            setInterval(function () {
-                if(ontwo ===1 ) ticktwo++;
-                console.log('Tick: ' + ticktwo);
-            }, 1000);
-        });
-
-        this.el.addEventListener('mouseleave', function () {
-            ontwo = 0;
-            ticktwo = 0;
-            console.log('Tick: ' + ticktwo);
-        });
-    },
-    tick: function() {
-        var data= this.data;
-        if(ticktwo>=4) {
-            data.target.emit(data.aevent);
-            data.target2.emit(data.aevent);
-            data.target3.emit(data.aevent);
-        }
-    }
-});
